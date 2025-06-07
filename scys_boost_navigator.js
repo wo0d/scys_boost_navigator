fcd6bd3fdee65a63fa91752add81ebe3d42b7cda
Rand0mWalk <wo0d@protonmail.com>
2025-06-07 09:15:21 +0800

    fix namespace

--- scys_boost_navigator.js
+++ scys_boost_navigator.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         【生财有术】官网导航栏增强
-// @namespace    http://tampermonkey.net/
+// @namespace    Violentmonkey Scripts
 // @version      1.1
 // @description  在生财有术官网导航栏添加收藏作者下拉菜单
 // @author       Rand0mWalk
