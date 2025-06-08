9d3ecdb1235143c0107136f7496a559efe282e3c
wo0d <wo0d@protonmail.com>
2025-06-08 11:00:49 +0800

    little update

--- scys_boost_navigator.js
+++ scys_boost_navigator.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         【生财有术】官网导航栏增强
 // @namespace    Violentmonkey Scripts
-// @version      1.2.4
+// @version      1.2.5
 // @description  在生财有术官网导航栏添加收藏作者下拉菜单
 // @author       Rand0mWalk
 // @match        https://scys.com/*
 // @exclude      https://scys.com/view/docx/*
-// @updateURL    https://raw.githubusercontent.com/wo0d/scys_boost_navigator/main/scys_boost_navigator.js
-// @downloadURL  https://raw.githubusercontent.com/wo0d/scys_boost_navigator/main/scys_boost_navigator.js
+// @updateURL    https://raw.githubusercontent.com/wo0d/scys_boost_navigator/refs/heads/main/scys_boost_navigator.js
+// @downloadURL  https://raw.githubusercontent.com/wo0d/scys_boost_navigator/refs/heads/main/scys_boost_navigator.js
 // @grant        none
 // ==/UserScript==
 
